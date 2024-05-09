6e09089e89235573d4304c19dc1c0accceb109c6
Matheus Vieira <matheus.fvp@outlook.com>
2024-05-09 19:44:26 -0300

    UserController

---
+++ user/src/main/java/com/mfvp/user/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.mfvp.user.controllers;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mfvp.user.dtos.UserRecordDto;
+import com.mfvp.user.models.UserModel;
+import com.mfvp.user.services.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto) {
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userRecordDto, userModel);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(userService.save(userModel));
+    }
+    
+}
