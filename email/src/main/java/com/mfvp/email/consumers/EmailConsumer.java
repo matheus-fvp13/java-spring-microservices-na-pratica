65704721e5b6e23e723081ef50952e91109c4b56
Matheus Vieira <matheus.fvp@outlook.com>
2024-05-09 21:08:07 -0300

    EmailConsumer created

---
+++ email/src/main/java/com/mfvp/email/consumers/EmailConsumer.java
@@ -0,0 +1,13 @@
+package com.mfvp.email.consumers;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+    @RabbitListener(queues = "${broker.queue.email.name}")
+    public void listenEmailQueue(@Payload String message) {
+        System.out.println("Email: " + message);
+    }
+}
